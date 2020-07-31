70b0174d5abc223a7daba12b806332aaf4041935
Bera <berakoc97@gmail.com>
2020-07-31 15:03:11 +0300

    📦: NEW: Implemented Footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './Footer.css'
 
 export default class Footer extends Component {
     render() {
         return (
             <div id="Footer">
-                Footer
+                Created by <a href="https://github.com/MuhammedBeraKoc/" target="_blank"> Bera </a> with ❤️
             </div>
         )
     }
--- client/src/components/native/CardList.tsx
+++ client/src/components/native/CardList.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class CardList extends Component {
     render() {
         return (
-            <div>
+            <div className="cardList">
                 CardList
             </div>
         )
